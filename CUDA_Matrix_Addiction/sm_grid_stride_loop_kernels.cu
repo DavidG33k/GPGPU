35acd16697507032341bb917060116ef2bb2de9c
DavidG33k <davide.gena@gmail.com>
2021-10-18 16:08:00 +0200

    Update
    Added sm_grid_stride_loop_kernels.cu and um_monolithic_kernels.cu.
    
    TO FIX: All versions don't work with big size matrices

--- CUDA_Matrix_Addiction/sm_grid_stride_loop_kernels.cu
+++ CUDA_Matrix_Addiction/sm_grid_stride_loop_kernels.cu
@@ -1,7 +1,7 @@
 /**
 GPGPU assignment 1: Matrix Addition in CUDA - Standard Memory/Grid Stride Loop Kernels version
     @file sm_grid_stride_loop_kernels.cu
-    @author Canonaco Martina @author Gena Davide
+    @author Canonaco Martina @author Gena Davide @author Morello Michele
     @version 13 October 2021 
 *
 Let A and B be the matrices of double-precision floating-point numbers to be added,
@@ -13,3 +13,69 @@ Implement four versions of the matrix addition application in CUDA using:
     - Unified-Memory/Monolithic-Kernels;
     - Unified-Memory/Grid-Stride-Loop-Kernels.
 */
+
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+#define M 5
+#define N 5
+
+__global__
+void add(double *A, double *B, double *C)
+{
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+
+  for (int i = index; i < M*N; i += stride)
+    C[i] = A[i] + B[i];
+}
+
+
+
+int main()
+{
+    double size = M * N * sizeof(double);
+
+    double A[M][N];
+    double B[M][N];
+    double C[M][N];
+
+    double *dev_A, *dev_B, *dev_C;
+
+    cudaMalloc((void**)&dev_A, size);
+    cudaMalloc((void**)&dev_B, size);
+    cudaMalloc((void**)&dev_C, size);
+
+    for(int i=0; i<M; i++)
+        for(int j=0; j<N; j++) {
+            A[i][j] = 1.0f;
+            B[i][j] = 2.0f;
+            C[i][j] = 0.0f;
+        }
+
+    cudaMemcpy(dev_A, A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_B, B, size, cudaMemcpyHostToDevice);
+
+
+    int blockSize = 256;
+    int numBlocks = (M * N + blockSize - 1) / blockSize;
+    add<<<numBlocks, blockSize>>>(dev_A, dev_B, dev_C);
+
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(C, dev_C, size, cudaMemcpyDeviceToHost);
+
+    for(int i=0; i<M; i++) {
+      for (int j=0; j<N; j++) {
+        cout << C[i][j] << " ";
+      }
+    cout<<endl;
+    }
+
+    cudaFree(dev_A);
+    cudaFree(dev_B);
+    cudaFree(dev_C);
+
+    return 0;
+}
--- CUDA_Matrix_Addiction/um_monolithic_kernels.cu
+++ CUDA_Matrix_Addiction/um_monolithic_kernels.cu
@@ -14,3 +14,91 @@ Implement four versions of the matrix addition application in CUDA using:
     - Unified-Memory/Grid-Stride-Loop-Kernels.
 */
 
+#include<iostream>
+#include<math.h>
+using namespace std;
+#define M 5
+#define N 5
+
+void matrixInit(float A[][N], float value)
+{
+    for(int i=0; i<M; i++)
+        for(int j=0; j<N; j++)
+            A[i][j] = value;
+}
+
+__global__
+void matrixAdd(float* A, float* B, float* C)
+{
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+    int index = col + row * N;
+
+    if (col < N && row < M) {
+        C[index] = A[index] + B[index];
+    }
+}
+
+void printMatrix(float A[][N])
+{
+    for(int i=0; i<M; i++)
+    {
+        for(int j=0; j<N; j++)
+            cout<<" "<<A[i][j]<<" ";
+        cout<<endl;
+    }
+}
+
+int main()
+{
+// variables declaration
+    dim3 dimBlock(16,16);
+    dim3 dimGrid(((N+dimBlock.x-1)/dimBlock.x),((M+dimBlock.y-1)/dimBlock.y));
+
+ //create and allocate matrix A, B and C
+    float* A; cudaMallocManaged(&A, M*N*sizeof(float));
+    float* B; cudaMallocManaged(&B, M*N*sizeof(float));
+    float* C; cudaMallocManaged(&C, M*N*sizeof(float));
+
+    float valori_A[M][N];
+    float valori_B[M][N];
+    float valori_C[M][N];
+
+    matrixInit(valori_A, 1.0f);
+    matrixInit(valori_B, 2.0f);
+    matrixInit(valori_C, 0.0f);
+
+    memcpy(&A[0], &valori_A[0][0], M*N*sizeof(float));
+    memcpy(&B[0], &valori_B[0][0], M*N*sizeof(float));
+    memcpy(&C[0], &valori_C[0][0], M*N*sizeof(float));
+
+    for(int i=0; i<M*N; i++)
+        cout << A[i] << " ";
+    cout<<endl;
+
+    for(int i=0; i<M*N; i++)
+        cout << B[i] << " ";
+    cout<<endl;
+
+    for(int i=0; i<M*N; i++)
+        cout << C[i] << " ";
+    cout<<endl;
+
+ 
+ //addiction operation and print results
+    matrixAdd<<<dimGrid, dimBlock>>>(A, B, C);
+    cudaDeviceSynchronize();
+
+    memcpy(&valori_C[0][0], &C[0], M*N*sizeof(float));
+
+    //printing resulting matrix C
+    cout<<endl<<"PRINT C final"<<endl;
+    printMatrix(valori_C);
+
+    cudaFree(A); 
+    cudaFree(B); 
+    cudaFree(C);
+    
+    return 0;
+}
